f351d97a34547bb34535e894086082c41076858a
Lucas Marques <luquinhas.mb07@gmail.com>
2021-08-04 15:20:33 -0300

    first commit this project

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$('.alvara').click(function(){
+    $('.menu .alvara-item').toggleClass('mostra');
+});
+$('.credencial').click(function(){
+    $('.menu .credencial-item').toggleClass('mostra');
+});
+$('.dashboard').click(function(){
+    $('.menu .dashboard-item').toggleClass('mostra');
+});
+$('.defesa').click(function(){
+    $('.menu .defesa-item').toggleClass('mostra');
+});
+$('.multa').click(function(){
+    $('.menu .multa-item').toggleClass('mostra');
+});
+$('.permissionario').click(function(){
+    $('.menu .permissionario-item').toggleClass('mostra');
+});
+$('.bc').click(function(){
+    $('.buscar').toggleClass('mostra');
+});
+$('.cat').click(function(){
+    $('.formulario').toggleClass('mostra');
+});
+$('.upmult').click(function(){
+    $('.uploadArquivo').toggleClass('mostra');
+});
+$('.dash_mult').click(function(){
+    $('.grafico').toggleClass('mostra');
+});
+
